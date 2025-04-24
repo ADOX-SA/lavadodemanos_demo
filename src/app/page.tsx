75de161ba8b2b6e3d53d3f4aace587239d40d087
Ivorra-1996 <ivorrajosemaria@gmail.com>
2025-04-24 18:02:03 -0300

    refactor: Simplificar la lógica de inicialización y manejo de inactividad en el proceso de detección

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -134,9 +134,7 @@ export default function Home() {
   
       if (isValid && !stepConfirmed) {
         console.log("✅ Paso válido detectado, inicializando...");
-        if (currentStep < labels.length - 1) {
-          setInitializing(true);
-        }
+        setInitializing(true);
         setStepConfirmed(true); // Confirmamos este paso
         startTimer(); // ⏱️ Iniciamos el temporizador para validarlo
       }
@@ -154,32 +152,27 @@ export default function Home() {
       }
     } else {
       // 📉 Si no se detecta nada, acumulamos frames sin manos
-      if (!initializing) return;
-  
-      console.log("👋 No se detectan manos, acumulando frames vacíos");
-      setConsecutiveNoHandsFrames((prev) => Math.min(prev + 1, 5));
-  
-      if (consecutiveNoHandsFrames >= 5) {
-        console.log("🔁 Pausando por inactividad");
-        pauseTimer();
-        setStepConfirmed(false);
-        startCountdown(); // 🕒 Iniciamos cuenta regresiva para reiniciar
+      if (initializing) {
+        console.log("👋 No se detectan manos, acumulando frames vacíos");
+        setConsecutiveNoHandsFrames((prev) => Math.min(prev + 1, 5));
       }
     }
   }, [predicciones]);
 
   useEffect(() => {
-    setStepConfirmed(false);
+    // setStepConfirmed(false); Esto creo que esta de mas, ya que se reinicia al cambiar de paso.
     pauseTimer(); // Asegurar que el timer se reinicie al cambiar de paso
   }, [currentStep]);
 
   // Manejar reinicio por inactividad
   useEffect(() => {
-    if (consecutiveNoHandsFrames === 5 && !isCountdownActive && initializing) {
+    if (consecutiveNoHandsFrames >= 5 && !isCountdownActive && initializing) {
+      console.log("🔁 Pausando por inactividad");
       pauseTimer();
-      startCountdown(); // Iniciar cuenta regresiva de reinicio
+      setStepConfirmed(false);
+      startCountdown(); // 🕒 Iniciamos cuenta regresiva para reiniciar
     }
-  }, [consecutiveNoHandsFrames, isCountdownActive, initializing]);
+  }, [consecutiveNoHandsFrames, isCountdownActive, initializing, pauseTimer, startCountdown,]);
 
   // Resetea todo a los valores inciales.
   const resetProcess = () => {
